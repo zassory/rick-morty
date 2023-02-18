71b480576f4e25dc11d0fcfc76d3173d123fc9fa
zassory <nicolas.programador@gmail.com>
2023-02-17 23:55:05 -0300

    comunicacion con api correcta usando fetch y luego transformandola a json

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 
 function App() {
 
-  const reqApi = () => {
-    console.log("Clicking");
+  const reqApi = async () => {
+      const api = await fetch('https://rickandmortyapi.com/api/character');
+      const characterApi = await api.json();
+      console.log(characterApi);
   }
 
 
